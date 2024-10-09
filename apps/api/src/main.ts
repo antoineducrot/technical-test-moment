aa6606df45e93ea5577fd901670d3b9ece2c2ed0
Antoine Ducrot <ducrot.antoine.en@gmail.com>
2024-10-09 09:02:20 +0200

    chore(api/doc): add swagger support for the api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Moment API")
+    .setDescription("This is the API for the Moment app.")
+    .setVersion("0.0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/", app, document);
 
   await app.listen(4000);
 }
